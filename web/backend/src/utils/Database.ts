a0aef34e51853fe506f57248b520cfe878ccde31
Burak Karayağlı <hazewiooo@gmail.com>
2024-01-08 00:02:31 +0300

    guest movement initalization

--- web/backend/src/utils/Database.ts
+++ web/backend/src/utils/Database.ts
@@ -7,7 +7,7 @@ class Database {
   private static config = {
     user: 'sa',
     password: '123456',
-    server: 'UNKNOWN',
+    server: 'HAZE-DELL',
     database: 'ERŞAT_FACTORY',
     options: {
         encrypt: true,
