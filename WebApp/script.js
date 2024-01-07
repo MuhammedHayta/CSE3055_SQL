f66c1d2a5f7c88b7b2ccb75b83af4c2c3942b908
Burak Karayağlı <hazewiooo@gmail.com>
2024-01-07 17:10:07 +0300

    frontend

---
+++ WebApp/script.js
@@ -0,0 +1,13 @@
+function showPage(pageId) {
+  // Hide all pages
+  var pages = document.getElementsByClassName("page");
+  for (var i = 0; i < pages.length; i++) {
+    pages[i].style.display = "none";
+  }
+
+  // Show the selected page
+  var selectedPage = document.getElementById(pageId);
+  if (selectedPage) {
+    selectedPage.style.display = "block";
+  }
+}
